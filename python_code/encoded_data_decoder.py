457d46927f04c7e80923b1bb573bd8faeda27fe8
drorda <dror.d.dayan@gmail.com>
2020-05-04 18:04:52 +0300

    made code prettier

--- python_code/encoded_data_decoder.py
+++ python_code/encoded_data_decoder.py
@@ -9,6 +9,7 @@ from jpeg_bit_reader import JpegBitReader
 from jpeg_common import debug_print, number_of_components
 
 
+# lookup table you ugly bugly
 def zig_zag_index(k, n=8):
     # upper side of interval
     if k >= n * (n + 1) // 2:
@@ -40,15 +41,16 @@ def put_value_in_matrix_zigzag(matrix, value, index):
 
 
 class McuParsedDctComponents:
-    def __init__(self):
-        self.raw_mcus = {i: [] for i in range(1, 4)}  # TODO fix indices
-        self.dequantized_mcus = {i: [] for i in range(1, 4)}
-        self.mcus_after_idct = {i: [] for i in range(1, 4)}
+    def __init__(self, component_ids):
+        self.raw_mcus = {i: [] for i in component_ids}
+        self.dequantized_mcus = {i: [] for i in component_ids}
+        self.mcus_after_idct = {i: [] for i in component_ids}
 
     def add_mcu(self, comp_id, mcu):
         self.raw_mcus[comp_id].append(mcu)
 
 
+# DROR: you will not exist
 class UnsmearedMcu:
     def __init__(self, color_components):
         self.color_components = color_components
@@ -59,69 +61,65 @@ class RawDataDecoder:
         self.raw_data = raw_data
         self.jpeg_decode_metadata = jpeg_decode_metadata
         self._decoded_mcu_list = []
+        # DROR: change alg and remove:
         self._unsmeared_mcu_matrix = {}  # will be (i,j) -> triplet of 8*8 numpy matrix of ycbcr
         self._rgb_matrix = {}  # will be (i,j) ->triplet 8*8 numpy matrix of rgbs
 
+        # DROR: we live in the YCbCr to RGB function
         self.Cred = 0.299
         self.Cgreen = 0.587
         self.Cblue = 0.114
 
-    def read_raw_mcus(self, bit_reader, components, n_mcu_horiz, n_mcu_vert, restart_interval):
-        idx = 0
-        rst_idx = 0
-        prev_dc_value = 0
-        while idx < n_mcu_horiz * n_mcu_vert:
-            # if idx > 0:
-            #     break
-
-            if restart_interval is not None and idx > 0 and (idx % restart_interval == 0):
-                # We expect RSTm
-                bit_reader.align()
-                marker_mark = bit_reader.read_bits_as_int(8)
-                if marker_mark != 0xFF:
-                    raise Exception("Expected RST marker during data scan, no marker appears")
-                marker_type = bit_reader.read_bits_as_int(8)
-                if marker_type != 0xd0 + rst_idx:
-                    if marker_type & 0xf0 == 0xd0:
-                        raise Exception(f"Expected RST marker is wrong. Expected {rst_idx} and got {marker_type & 0x0F}")
-                    else:
-                        raise Exception(f"Expected RST marker and got different marker of type {marker_type}")
-                prev_dc_value = 0
-                rst_idx = rst_idx + 1 if rst_idx < 7 else 0
+    @staticmethod
+    def handle_restart_interval(bit_reader, rst_idx):
+        # We expect RSTm
+        bit_reader.align()
+        marker_mark = bit_reader.read_bits_as_int(JpegBitReader.BYTE_SIZE)
+        if marker_mark != 0xFF:
+            raise Exception("Expected RST marker during data scan, no marker appears")
+        marker_type = bit_reader.read_bits_as_int(JpegBitReader.BYTE_SIZE)
+        if marker_type != 0xd0 + rst_idx:
+            if 0xd0 <= marker_type <= 0xd7:
+                raise Exception(f"Expected RST marker is wrong. Expected {rst_idx} and got {marker_type & 0x0F}")
             else:
-                debug_print(f"Decoding MCU #{idx}")
-                parsed_mcu = McuParsedDctComponents()
+                raise Exception(f"Expected RST marker and got different marker of type {marker_type}")
+        rst_idx = (rst_idx + 1) % 8
+        return rst_idx
+
+    # DROR: mcu == minimal cyber unit
+    def huffman_decode_mcus(self, bit_reader, components, n_mcu_horiz, n_mcu_vert, restart_interval):
+        rst_idx = 0
+        prev_dc_value = {k: 0 for k in components.keys()}
+        for mcu_idx in range(n_mcu_horiz * n_mcu_vert):
+            if restart_interval is not None and mcu_idx > 0 and (mcu_idx % restart_interval == 0):
+                prev_dc_value = {k: 0 for k in components.keys()}
+                rst_idx = self.handle_restart_interval(bit_reader, rst_idx)
+            else:
+                debug_print(f"Decoding MCU #{mcu_idx}")
+                parsed_mcu = McuParsedDctComponents(components.keys())
 
                 for (comp_id, comp) in components.items():
-                    for comp_no in range(comp.number_of_instances_in_mcu):
-                        decoded_mcu = self.decode_component_mcu(bit_reader, comp.ac_huffman_table, comp.dc_huffman_table)
+                    for _ in range(comp.number_of_instances_in_mcu):
+                        decoded_mcu = self.decode_component_in_mcu(bit_reader, comp.ac_huffman_table,
+                                                                   comp.dc_huffman_table, prev_dc_value, comp_id)
                         parsed_mcu.add_mcu(comp_id, decoded_mcu)
                 self._decoded_mcu_list.append(parsed_mcu)
-
-            idx += 1
 
     def de_quantize(self, components):
         debug_print("Beginning De-quantization")
         for decoded_mcu in self._decoded_mcu_list:
             for (comp_id, comp) in components.items():
                 quantization_table = comp.quantization_table
-                debug_print("Quantization table:")
-                debug_print(quantization_table)
                 for comp_mcu in decoded_mcu.raw_mcus[comp_id]:
-                    debug_print("Quantized:")
-                    debug_print(comp_mcu)
                     de_quantized_mcu = np.multiply(quantization_table, comp_mcu)
-                    debug_print("Dequantized:")
-                    debug_print(de_quantized_mcu)
                     decoded_mcu.dequantized_mcus[comp_id].append(de_quantized_mcu)
+        debug_print("Finish De-quantization")
 
-    def do_idct(self, component_keys):
+    def inverse_dct(self, component_keys):
         for decoded_mcu in self._decoded_mcu_list:
             for comp_id in component_keys:
-                for comp_mcu in decoded_mcu.raw_mcus[comp_id]:
+                for comp_mcu in decoded_mcu.dequantized_mcus[comp_id]:
                     after_idct = scipy.fft.idct(comp_mcu)
-                    debug_print("After IDCT:")
-                    debug_print(after_idct)
                     decoded_mcu.mcus_after_idct[comp_id].append(after_idct)
 
     # The jpeg MCUs are simply placed one after the other, therefore decoding them must be in order.
@@ -130,7 +128,7 @@ class RawDataDecoder:
     #   1. Decode the huffman.
     #   2. Convert the bits to dc and ac values (will be further explained by Gal later)
     #   3. Un quantize
-    #   4. Un DCT
+    #   4. inverse dct
     #   5. Convert to rgb
     def decode(self):
 
@@ -141,10 +139,12 @@ class RawDataDecoder:
         n_mcu_horiz = math.ceil(self.jpeg_decode_metadata.width / pixels_mcu_horiz)
         n_mcu_vert = math.ceil(self.jpeg_decode_metadata.height / pixels_mcu_vert)
 
-        self.read_raw_mcus(bit_reader, self.jpeg_decode_metadata.components_to_metadata, n_mcu_horiz, n_mcu_vert, self.jpeg_decode_metadata.restart_interval)
-        self.absoultify_dc_values()
+        self.huffman_decode_mcus(bit_reader, self.jpeg_decode_metadata.components_to_metadata, n_mcu_horiz, n_mcu_vert,
+                                 self.jpeg_decode_metadata.restart_interval)
+
         self.de_quantize(self.jpeg_decode_metadata.components_to_metadata)
-        self.do_idct(self.jpeg_decode_metadata.components_to_metadata.keys())
+        self.inverse_dct(self.jpeg_decode_metadata.components_to_metadata.keys())
+        self.generate_pixel_map()
         self.unsmear_mcus(self.jpeg_decode_metadata.components_to_metadata, n_mcu_horiz, n_mcu_vert, pixels_mcu_horiz,
                           pixels_mcu_vert)
         self._to_rgb()
@@ -154,78 +154,85 @@ class RawDataDecoder:
                                   height=n_mcu_vert * pixels_mcu_vert)
 
         '''
-        
         TODO:
         * merge unsmeared mcu to a single matrix per color component
         * finish bmp scanning with bigbig matrix
-        # don't forget to absolutify dc values
         '''
 
     @staticmethod
     def decode_with_huffman(bit_reader, huff_tree):
-        debug_print("Reading bits: ", newline=False)
         while not huff_tree.is_leaf():
             next_bit = bit_reader.get_bits_as_bool_list(1)[0]
-            debug_print('1' if next_bit else '0', newline=False)
             huff_tree = huff_tree.get_kid(1 if next_bit else 0)
         code = huff_tree.get_value()
-        debug_print("")
         return code
 
-    def decode_component_mcu(self, bit_reader, ac_huffman, dc_huffman):
-        dc_tree = dc_huffman.get_tree()
+    # How is the DC value encoded: You first use the huffman tree and get a value. We call it DC code. funny name
+    # right? Then, you read DC code bits! Like if DC code is 5 you read 5 bits!!! This guy is called
+    # additional_bits. You then go to table 5 and look hard hard at the row with DC code. You calculate the
+    # offset (good luck with that..) and get DC value. If you want the full details go to this website:
+    # https://www.impulseadventure.com/photo/jpeg-huffman-coding.html, Table 5 - Huffman DC Value Encoding
+    # Then you remember the DC value is relative to the previous one in the same component so you do the funny
+    # offset calculation and Voilà! Your DC value is ready :)
+    def decode_dc_for_component_in_mcu(self, dc_tree, bit_reader, prev_dc_value, comp_id):
         debug_print('Decoding DC')
         dc_code = self.decode_with_huffman(bit_reader, dc_tree)
         debug_print('DC code is ', hex(dc_code))
-        assert (dc_code <= 0x0F)
+        assert 0 <= dc_code <= 0x0F, "dc_code must be 0 to 16"
         additional_bits = bit_reader.read_bits_as_int(dc_code)
-
         dc_value = value_encoding(dc_code, additional_bits)
+        dc_value += prev_dc_value[comp_id]
+        prev_dc_value[comp_id] = dc_value
+        return dc_value
 
-        decoded_idx = 0
-
-        decoded_mcu = np.zeros((8, 8))
-        put_value_in_matrix_zigzag(decoded_mcu, dc_value, decoded_idx)
-        decoded_idx += 1
+    # First you get a value from the tree like in the DC.
+    # Now you got a byte (8 bits). You take the first 4 and call them run_length and the other 4 is name size.
+    # Run length is the number of zeros until the next non-zero value, and size we will see.
+    # BUT if run_length == size == 0 is called EOB then it says OK no more non-zero values from now on all is zero
+    # that's it so we stop (we inited the array to zeros).
+    # Another but -- if run_length == 15 and size == 0 is called ZRL means that now come 16 zeros and then continue
+    # like business as usual everything good.
+    # Otherwise you put run_length zeros zigzagly and then you do again the table 5 SHTIK with size bits you read,
+    # which are like additional_bits in the DC explanation. Thus fiasco gives you an AC value which is != 0.
+    def decode_ac_for_component_in_mcu(self, ac_tree, bit_reader, decoded_component_data):
         debug_print('Decoding AC')
-
+        decoded_idx = 1
         while decoded_idx < 64:
-            ac_tree = ac_huffman.get_tree()
-
             ac_code = self.decode_with_huffman(bit_reader, ac_tree)
-            run_length = (ac_code & 0xF0) >> 4
-            size = ac_code & 0x0F
+            run_length = (ac_code & 0xf0) >> 4
+            size = ac_code & 0x0f
             debug_print(f"AC code = {ac_code}, which is ({run_length}, {size})")
             if run_length == 0 and size == 0:
                 # This is EOB. No more (we start with a zero matrix)
                 debug_print("EOB")
                 break
             elif run_length == 15 and size == 0:
-                decoded_idx += 16
-                debug_print("ZRL")
                 # This is ZRL
+                debug_print("ZRL")
+                decoded_idx += 16
                 continue
             else:
-                if run_length > 0:
-                    debug_print(f"Putting {run_length} zero AC values")
-                for i in range(run_length):
-                    put_value_in_matrix_zigzag(decoded_mcu, 0, decoded_idx)
-                    decoded_idx += 1
+                debug_print(f"Putting {run_length} zero AC values")
+                decoded_idx += run_length
                 value_aux = bit_reader.read_bits_as_int(size)
                 debug_print(f"additional bits = {value_aux}")
 
                 ac_value = value_encoding(size, value_aux)  # This is again table 5!!
                 debug_print(f"AC value = {ac_value}")
-                put_value_in_matrix_zigzag(decoded_mcu, ac_value, decoded_idx)
+                put_value_in_matrix_zigzag(decoded_component_data, ac_value, decoded_idx)
                 decoded_idx += 1
 
-        debug_print('Done decoding MCU')
+    def decode_component_in_mcu(self, bit_reader, ac_huffman, dc_huffman, prev_dc_value, comp_id):
+        decoded_component_data = np.zeros((8, 8))
 
-        debug_print(decoded_mcu)
-        return decoded_mcu
+        dc_value = self.decode_dc_for_component_in_mcu(dc_huffman.get_tree(), bit_reader, prev_dc_value, comp_id)
+        put_value_in_matrix_zigzag(decoded_component_data, dc_value, 0)
 
-    def absoultify_dc_values(self):
-        print("Yo what about me?")
+        self.decode_ac_for_component_in_mcu(ac_huffman.get_tree(), bit_reader, decoded_component_data)
+
+        debug_print('Done decoding component')
+        debug_print(decoded_component_data)
+        return decoded_component_data
 
     def unsmear_mcus(self, components, n_mcu_horiz, n_mcu_vert, pixels_mcu_horiz, pixels_mcu_vert):
         num_unsmeared_horiz = pixels_mcu_horiz // 8
